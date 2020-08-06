d672838efacba01ccaf5c7ce45f44d77ae0d9996
RasVadim <rasstriginv@gmail.ru>
2020-08-06 17:39:15 +0400

    Test task for CosySoft debug6

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b334bb3706921c79fc7f66db6adf425d.js"
+  "/precache-manifest.32a1ad4d15ea67f016903ae2734a8e51.js"
 );
 
 self.addEventListener('message', (event) => {
