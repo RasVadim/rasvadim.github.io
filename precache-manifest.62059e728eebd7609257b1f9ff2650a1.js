cdfcaee52f743c54a20a133798bec1f6c5afc3ed
RasVadim <rasstriginv@gmail.ru>
2020-08-06 14:55:23 +0400

    Test task for CosySoft

---
+++ precache-manifest.62059e728eebd7609257b1f9ff2650a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545b7b58140b3d0063500afafd36831f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0bb6622b5cb459f5e53",
+    "url": "/static/css/main.275842d8.chunk.css"
+  },
+  {
+    "revision": "fbf00df6bacece0f51b2",
+    "url": "/static/js/2.e3d2580c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3d2580c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bb6622b5cb459f5e53",
+    "url": "/static/js/main.17f4c9f3.chunk.js"
+  },
+  {
+    "revision": "95e33ed823139bd7e533",
+    "url": "/static/js/runtime-main.bd7eb189.js"
+  }
+]);
